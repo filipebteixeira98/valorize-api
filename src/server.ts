53b5590490dc01e3f0e9c9c0c29868fdba2d1daa
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-02-02 02:43:51 -0300

    feat: added route that allows to create a new user

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import 'reflect-metadata';
-
 import express from 'express';
 
 import './database';
 
+import { router } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: true });
-});
+app.use(router);
 
 app.listen(3333, () => {
   console.log('🚀 Server is running at http://localhost:3333');
