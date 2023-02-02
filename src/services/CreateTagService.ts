6c9cb0d8a601ce91629ab66fd58a4b0552041b61
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-02-02 03:50:53 -0300

    chore: added service to create tag

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from 'typeorm';
+
+import { TagsRepositories } from '../repositories/TagsRepositories';
+
+class CreateTagService {
+  async execute(name: string) {
+    const tagsRepository = getCustomRepository(TagsRepositories);
+
+    if (!name) {
+      throw new Error('Name is required!');
+    }
+
+    const tagAlreadyExists = await tagsRepository.findOne({ name });
+
+    if (tagAlreadyExists) {
+      throw new Error('Tag already exists!');
+    }
+
+    const tag = tagsRepository.create({ name });
+
+    await tagsRepository.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
